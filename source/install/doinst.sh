dbc1096adda924de55f3dd32324df41b4bfd54f6
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-06-13 13:04:49 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -3,4 +3,4 @@ DOCROOT="/usr/local/emhttp/plugins/dwmemtester"
 chmod 755 $DOCROOT/scripts/*
 chmod 755 /usr/bin/memtester
 chmod 755 /usr/bin/memtester-runner
-mkdir -p /var/lib/memtester
+mkdir -p /var/log/memtester
