086ca814b519e30da0f78034f2d234551ba4ef30
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-06-13 12:25:31 +0200

    Update dwmemtester_log.php

--- source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_log.php
+++ source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_log.php
@@ -2,7 +2,7 @@
 if(file_exists("/var/log/memtester/log")) {
     $mem_log = file_get_contents("/var/log/memtester/log");
     if(!empty($mem_log)) {
-        echo("<pre class='memlog'>".$mem_log."</pre>");
+        echo("<pre class='memlog'>".htmlspecialchars($mem_log)."</pre>");
     } else {
         echo("<pre class='memlog'></pre>");
     }
