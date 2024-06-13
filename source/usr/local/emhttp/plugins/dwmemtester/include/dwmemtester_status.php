6cb646c68c774209e0d1e441e9325c456ec5a6ce
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-06-13 15:52:36 +0200

    changes

--- source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
+++ source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
@@ -1,4 +1,6 @@
 <?
 $mem_running = trim(htmlspecialchars(shell_exec( "if pgrep -x memtester >/dev/null 2>&1; then echo YES; else echo NO; fi" ) ?? "-"));
-echo("RUNNING:".$mem_running);
+$memlog_size = trim(file_exists("/var/lib/memtester/log") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/log"))) : "");
+$errlog_size = trim(file_exists("/var/lib/memtester/errlog") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/errlog"))) : "");
+echo("RUNNING:".$mem_running.",".$memlog_size.",".$errlog_size);
 ?>
