5c61db7158749dfbdd8d6b281f05744b1e6bceb9
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-06-13 15:55:57 +0200

    Update dwmemtester_status.php

--- source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
+++ source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
@@ -1,4 +1,27 @@
 <?
+function humanFileSize($sizeObj,$unit="") {
+    try {
+        $size = intval($sizeObj);
+        if($size) {
+            if( (!$unit && $size >= 1<<40) || $unit == "TB")
+                return number_format($size/(1<<40),2)." TB";
+            if( (!$unit && $size >= 1<<30) || $unit == "GB")
+                return number_format($size/(1<<30),2)." GB";
+            if( (!$unit && $size >= 1<<20) || $unit == "MB")
+                return number_format($size/(1<<20),2)." MB";
+            if( (!$unit && $size >= 1<<10) || $unit == "KB")
+                return number_format($size/(1<<10),2)." KB";
+            return number_format($size)." B";
+        } else {
+            return "-";
+        }
+    } catch (Throwable $e) { // For PHP 7
+        return "-";
+    } catch (Exception $e) { // For PHP 5
+        return "-";
+    }
+}
+
 $mem_running = trim(htmlspecialchars(shell_exec( "if pgrep -x memtester >/dev/null 2>&1; then echo YES; else echo NO; fi" ) ?? "-"));
 $memlog_size = trim(file_exists("/var/lib/memtester/log") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/log"))) : "");
 $errlog_size = trim(file_exists("/var/lib/memtester/errlog") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/errlog"))) : "");
