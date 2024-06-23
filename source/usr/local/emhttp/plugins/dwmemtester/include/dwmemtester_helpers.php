2265f5af688c46d25d6a77f78fd6faa01c01f705
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-06-23 10:13:17 +0200

    changes

--- source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_helpers.php
+++ source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_helpers.php
@@ -17,18 +17,18 @@
  * included in all copies or substantial portions of the Software.
  *
  */
-function humanFileSize($sizeObj,$unit="") {
+function mem_humanFileSize($sizeObj,$unit="") {
     try {
         $size = intval($sizeObj);
         if($size) {
-            if( (!$unit && $size >= 1<<40) || $unit == "TB")
-                return number_format($size/(1<<40),2)." TB";
-            if( (!$unit && $size >= 1<<30) || $unit == "GB")
-                return number_format($size/(1<<30),2)." GB";
-            if( (!$unit && $size >= 1<<20) || $unit == "MB")
-                return number_format($size/(1<<20),2)." MB";
-            if( (!$unit && $size >= 1<<10) || $unit == "KB")
-                return number_format($size/(1<<10),2)." KB";
+            if( (!$unit && $size >= 1000000000000) || $unit == "TB")
+                return number_format(($size/1000000000000),2)." TB";
+            if( (!$unit && $size >= 1000000000) || $unit == "GB")
+                return number_format(($size/1000000000),2)." GB";
+            if( (!$unit && $size >= 1000000) || $unit == "MB")
+                return number_format(($size/1000000),2)." MB";
+            if( (!$unit && $size >= 1000) || $unit == "KB")
+                return number_format(($size/1000),2)." KB";
             return number_format($size)." B";
         } else {
             return false;
--- source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
+++ source/usr/local/emhttp/plugins/dwmemtester/include/dwmemtester_status.php
@@ -26,13 +26,13 @@ $mem_disk_util = "";
 $mem_highram = "";
 
 if(!empty($_GET["getfs"]) && $_GET["getfs"] === "yes" ) {
-    $mem_log_size = trim(file_exists("/var/lib/memtester/log") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/log"))) : "");
-    $mem_errlog_size = trim(file_exists("/var/lib/memtester/errlog") ? htmlspecialchars(humanFileSize(filesize("/var/lib/memtester/errlog"))) : "");
+    $mem_log_size = trim(file_exists("/var/lib/memtester/log") ? htmlspecialchars(mem_humanFileSize(filesize("/var/lib/memtester/log"))) : "");
+    $mem_errlog_size = trim(file_exists("/var/lib/memtester/errlog") ? htmlspecialchars(mem_humanFileSize(filesize("/var/lib/memtester/errlog"))) : "");
 }
 
-$mem_disk_util = trim(htmlspecialchars(shell_exec("df --output=pcent /var/lib/memtester 2>/dev/null | tr -dc '0-9' 2>/dev/null") ?? ""));
+$mem_disk_util = htmlspecialchars(trim(shell_exec("df --output=pcent /var/lib/memtester 2>/dev/null | tr -dc '0-9' 2>/dev/null") ?? ""));
 if(!empty($mem_disk_util)) { $mem_highram = ($mem_disk_util < 90) ? "NO" : "YES"; }
 
-$mem_running = trim(htmlspecialchars(shell_exec( "if pgrep -x memtester >/dev/null 2>&1; then echo YES; else echo NO; fi" ) ?? "-"));
+$mem_running = htmlspecialchars(trim(shell_exec( "if pgrep -x memtester >/dev/null 2>&1; then echo YES; else echo NO; fi" ) ?? "-"));
 echo("RUNNING:".$mem_running.",".$mem_highram.",".$mem_log_size.",".$mem_errlog_size);
 ?>
